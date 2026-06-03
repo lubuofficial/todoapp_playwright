096a9e0340b7acfbf0a9b3f3b9a5d45f7b4c777e
lubuofficial <lilly9101@gmail.com>
2026-06-03 17:19:56 +0700

    Initial commit

--- playwright.config.js
+++ playwright.config.js
@@ -31,6 +31,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    launchOptions: {
+      headless: false,
+      slowMo: 300,
+    },
   },
 
   /* Configure projects for major browsers */
